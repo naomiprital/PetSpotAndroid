3b71ef88c38709d818045fb09a5f88c8c701af4e
YuvalPechter <yuval30.07.2003@gmail.com>
2026-05-01 14:51:15 +0300

    fix: merge conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
     alias(libs.plugins.navigation.safeargs)
     alias(libs.plugins.gms.google.services)
@@ -31,6 +32,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
     buildFeatures {
         viewBinding = true
@@ -66,12 +70,6 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.storage)
-
-    implementation(libs.gms.play.services.base)
-    implementation(libs.gms.play.services.auth)
-
-    implementation(libs.gson)
-}
     implementation(libs.gson)
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.navigation.safeargs) apply false
     alias(libs.plugins.gms.google.services) apply false
